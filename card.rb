9e612898b63c36e697676c15d46a18610113b911
Alexandr Baev <aleksandr3103999@gmail.com>
2019-06-27 20:47:29 +0300

    instal card

---
+++ card.rb
@@ -0,0 +1,4 @@
+suit = ["Crest", "Pika", "Cherva", "Buba"] #массив мастей
+number_card = ["2", "3", "4", "5", "6", "7" "8", "9", "10", "V", "D", "K", "T", "J"] #Массив номеров карт
+
+puts "#{suit.sample} of #{number_card.sample}"
